3c042bdc01b5cc93a4db1fe7bdac6013c39d710e
YCKJ3809 <luruyi@cloudwalk.com>
2020-09-08 18:21:48 +0800

    try reset111

--- src/hello.java
+++ src/hello.java
@@ -2,6 +2,7 @@ public class hello {
     public static void main(String args[]){
         System.out.println("Hi 学习使我快乐");
         System.out.println("这是newbranchone");
+        System.out.println("xianzaikankan");
     }
 }
 
